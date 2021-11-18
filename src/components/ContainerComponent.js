ddf3497314a3a0253c6ea0df030146c18ac8fd09
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-17 21:21:06 -0600

    initial set up of files

---
+++ src/components/ContainerComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Crypto from './Crypto'
+
+export default function ContainerComponent() {
+    return (
+        <div>
+            <Crypto />
+        </div>
+    )
+}
---
+++ src/components/Crypto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Crypto() {
+    return (
+        <div>
+            This is from the Crypto component
+        </div>
+    )
+}
